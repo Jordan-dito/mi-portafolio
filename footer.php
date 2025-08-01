5ff20de93e6b88599f9da41dcee59cb0655c017b
Jordan-dito <75227714+Jordan-dito@users.noreply.github.com>
2025-07-31 23:38:13 -0500

    Primer commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+  <footer id="footer" class="footer dark-background">
+    <div class="container">
+      <h3 class="sitename">Personal</h3>
+      <p>Desarrollador Fullstack y móvil desde Ecuador, disponible para trabajar en remoto. Comprometido con la calidad, la mejora continua y la colaboración efectiva en equipos internacionales.</p>
+      <div class="social-links d-flex justify-content-center">
+
+        <a href="https://www.facebook.com/robert.lapo.75"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/jordanrobere/"><i class="bi bi-instagram"></i></a>
+        <a href="www.linkedin.com/in/robert-jordan-lapo-malave-549767200"><i class="bi bi-linkedin"></i></a>
+      </div>
+      <div class="container">
+        <div class="copyright">
+          <span>Copyright</span> <strong class="px-1 sitename">Jordansito</strong> <span>All Rights Reserved</span>
+        </div>
+        <div class="credits">
+
+          Designed by <a href="https://bootstrapmade.com/">Jordansito</a>
+        </div>
+      </div>
+    </div>
+  </footer>
